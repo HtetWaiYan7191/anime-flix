1a0b1bab6e8efde1721d7267b4791e7f4ef430b2
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-06 16:16:11 +0630

    Set up browser router and provider in app level component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SearchPage from './pages/SearchPage'
+import Details from './pages/Details'
+import Airing from './pages/Airing'
+import Upcoming from './pages/Upcoming'
 
-const App = () => {
-  return (
-    <div className=' text-red-400'>
-      This is app 
-    </div>
-  )
-}
+const App = () => (
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/airing' element={<Airing/>}/>
+    <Route path='/upcoming' element={<Upcoming/>}/>
+    <Route path='/search:name' element={<SearchPage/>}></Route>
+    <Route path='details:id' element={<Details/>}></Route>
+  </Routes>
+);
 
 export default App
