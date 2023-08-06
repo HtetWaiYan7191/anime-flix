f803543ef3f26e0a3ecab261d7bc707ba96ae86e
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-06 17:04:21 +0630

    Add component to show airing anime list

--- src/pages/Airing.jsx
+++ src/pages/Airing.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Airing = () => {
-  return (
-    <div>
-      this is airing
-    </div>
-  )
-}
+  const allAnime = useSelector((state) => state.popular.value);
+  const airingAnime = allAnime.filter((anime) => anime.airing === true);
 
-export default Airing
+  return (
+    <section className='airing-section'>
+  <div className='airing-container flex flex-wrap gap-5'>
+    {airingAnime.map((anime) => {
+      return (
+        <div className='airing-card' key={anime.id}> {/* Make sure to add a unique key prop */}
+          <img src={anime.images.jpg.image_url} alt="" />
+        </div>
+      );
+    })}
+  </div>
+</section>
+
+  );
+};
+
+export default Airing;
--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Upcoming = () => {
-  return (
-    <div>
-      this is upcoming
-    </div>
-  )
-}
+  const allAnime = useSelector((state) => state.popular.value);
+  const upcomingAnime = allAnime.filter((anime) => anime.status === "Finished Airing");
+  console.log(upcomingAnime)
 
-export default Upcoming
+  return (
+    <section className="upcoming-section">
+      <div className="upcoming-container">
+        {upcomingAnime.map((anime) => (
+          <div className="upcoming-card">{anime.title}</div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Upcoming;
