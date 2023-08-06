12772e67e7af8117d375b07e2107aba347dde0eb
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-06 16:16:42 +0630

    Implement required pages for projects

---
+++ src/pages/Airing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Airing = () => {
+  return (
+    <div>
+      this is airing
+    </div>
+  )
+}
+
+export default Airing
---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchPopularAnime } from '../redux/popularSlice';
+import PopularAnimeCard from '../components/popularAnimeCard';
+import '../styles/Home.css'
+
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const popularAnime = useSelector((state) => state.popular.value)
+  console.log(popularAnime)
+  useEffect(() => {
+    dispatch(fetchPopularAnime())
+  },[])
+  return (
+    <section className='home-section'>
+      <div className='ads-trailer-container'>
+
+      </div>
+
+      <div className = 'home-container'>
+        {popularAnime.map((anime) => <PopularAnimeCard anime={anime} key={anime.mal_id}/>)}
+      </div>
+    </section>
+  )
+}
+
+export default Home
