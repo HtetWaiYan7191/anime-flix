3e2ecd54785cb129193807ed1f78df8725eb9a47
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-07 20:52:20 +0630

    Add airing page to show airing animes

--- src/pages/Airing.jsx
+++ src/pages/Airing.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAiringAnime } from "../redux/airingSlice";
 import '../styles/Airing.css'
+import { Link } from "react-router-dom";
 
 const Airing = () => {
   const airingAnime = useSelector((state) => state.airing.value)
@@ -12,13 +13,15 @@ const Airing = () => {
   })
 
   return (
-    <section className='airing-section'>
+    <section className='airing-section pt-28'>
   <div className='airing-container grid grid-cols-6 gap-5 p-[20px]'>
     {airingAnime.map((anime) => {
       return (
-        <div className='airing-card' key={anime.id}> {/* Make sure to add a unique key prop */}
+        <Link to={`/details/${anime.mal_id}`} key={anime.id}>
+        <div className='airing-card' > {/* Make sure to add a unique key prop */}
           <img src={anime.images.jpg.image_url} alt="" className=" object-fill h-[350px]" />
         </div>
+        </Link>
       );
     })}
   </div>
