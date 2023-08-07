ba589aec7fdbec65f9b0506c8e219f858b19be2e
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-07 20:50:19 +0630

    Update popularanimae card

--- src/components/PopularAnimeCard.jsx
+++ src/components/PopularAnimeCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../styles/animeCard.css'
+import { Link } from 'react-router-dom'
 
 const PopularAnimeCard = ({anime}) => {
   return (
+    <Link to = {`/details/${anime.mal_id}`}>
     <div className='popular-anime-card'>
       <img src={anime.images.jpg.image_url} alt={anime.title} className='' />
     </div>
+    </Link>
   )
 }
 
